dbdb713ab18b47adf4eb3fc9a27e70001916d967
Jennifer Zhan <jennifer.zhan.2015@gmail.com>
2024-08-07 01:30:02 -0400

    update google maps styles

--- components/cinema/cinema.tsx
+++ components/cinema/cinema.tsx
@@ -1,5 +1,5 @@
 "use client";
-// import { Card, CardBody } from "@nextui-org/react";
+import { Card, CardBody } from "@nextui-org/react";
 import { title } from "@/components/primitives";
 import { useState } from "react";
 import {
@@ -18,35 +18,38 @@ export default function Cinema() {
 
   return (
     <section className="mt-6 w-full">
-      {/* <Card */}
-      {/* isBlurred
+      <Card
+        isBlurred
         className="border-none bg-background/60 dark:bg-default-100/50 "
         shadow="sm"
-      > */}
-      {/* <CardBody className="flex items-center justify-center "> */}
-      <h1 className={title()}>Toronto Art-House Cinemas</h1>
-      <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY ?? ""}>
-        <div style={{ height: "40vh", width: "100%" }}>
-          <Map
-            zoom={12}
-            center={position}
-            mapId={process.env.NEXT_PUBLIC_GOOGLE_MAP_ID}
-          >
-            <Markers
-              points={cinemas}
-              setOpen={() => setOpen(true)}
-              setPoint={setPoint}
-            />
-            {open && (
-              <InfoWindow position={point} onCloseClick={() => setOpen(false)}>
-                <CinemaCard />
-              </InfoWindow>
-            )}
-          </Map>
-        </div>
-      </APIProvider>
-      {/* </CardBody> */}
-      {/* </Card> */}
+      >
+        <CardBody className="flex items-center justify-center ">
+          <h1 className={`${title()} pb-4`}>Art-House Cinemas</h1>
+          <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY ?? ""}>
+            <div style={{ height: "40vh", width: "100%" }}>
+              <Map
+                zoom={12}
+                center={position}
+                mapId={process.env.NEXT_PUBLIC_GOOGLE_MAP_ID}
+              >
+                <Markers
+                  points={cinemas}
+                  setOpen={() => setOpen(true)}
+                  setPoint={setPoint}
+                />
+                {open && (
+                  <InfoWindow
+                    position={point}
+                    onCloseClick={() => setOpen(false)}
+                  >
+                    <CinemaCard />
+                  </InfoWindow>
+                )}
+              </Map>
+            </div>
+          </APIProvider>
+        </CardBody>
+      </Card>
     </section>
   );
 }
