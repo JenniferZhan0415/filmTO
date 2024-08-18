764ae6022ea56a7d8463dfaa75a585df06151177
Jennifer Zhan <jennifer.zhan.2015@gmail.com>
2024-08-17 21:57:53 -0400

    code formatting and fix bugs in the articles page (#16)

--- components/userdashboard/user-dashboard.tsx
+++ components/userdashboard/user-dashboard.tsx
@@ -11,31 +11,40 @@ import UserSavedCard from "./user-saved-card";
 import ChangeTheme from "./change-theme";
 import UserInfo from "./user-info";
 
-import { title } from "@/components/primitives";
+import { title, Color } from "@/components/primitives";
 import { getUserByEmail } from "@/actions/user";
 
 export default function UserDashboard() {
   const { data: session, status } = useSession();
+  const { theme: globalTheme, setTheme: setGlobalTheme } = useTheme();
   const router = useRouter();
 
-  const { theme: globalTheme, setTheme: setGlobalTheme } = useTheme();
   const [theme, setTheme] = useState(globalTheme);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (status === "unauthenticated") {
+    // redirect for invalid session
+    if (status === "unauthenticated" || !session?.user?.email) {
       router.push("/login");
+
+      return;
     }
     // fetch user theme from db
     (async () => {
-      const user = await getUserByEmail(session?.user?.email!);
+      const user = await getUserByEmail(session.user?.email!);
 
       if (!user) {
         router.push("/login");
 
         return;
       }
-      setGlobalTheme(user.theme!);
-      setTheme(user.theme!);
+
+      // update theme
+      if (!user.theme) {
+        user.theme = "default";
+      }
+      setGlobalTheme(user.theme);
+      setTheme(user.theme);
     })();
   }, [status]);
 
@@ -44,21 +53,21 @@ export default function UserDashboard() {
     setTheme(globalTheme);
   }, [globalTheme]);
 
-  if (status === "loading") {
+  if (status === "loading" || loading) {
     return <Loading label="Authenticating..." />;
   }
 
   if (status === "authenticated") {
     return (
       <section className="flex flex-col items-center w-full px-2">
-        <h1 className={`${title({ color: theme })} pb-4 text-center`}>
+        <h1 className={`${title({ color: theme as Color })} pb-4 text-center`}>
           Welcome back, {session?.user?.name}!
         </h1>
         <div className="flex items-center mb-6 gap-4">
           <h4 className="text-default-500 ">
             Change the website theme by your favorite film
           </h4>
-          <UserEditLink />
+          <UserEditLink setLoading={setLoading} />
         </div>
         <div className="flex flex-col lg:justify-between md:flex-row md:justify-between sm:items-center  w-full h-full gap-4 mb-6 ">
           <div className="flex flex-col items-start min-w-fit justify-between">
